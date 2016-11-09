44342b98d6b37dc35734079939c52072b4dff7e7
Arkadii Berezkin <arkm4n@gmail.com>
2016-11-09 22:44:46 +1000

    Move image-upload component to a separate folder

--- image-upload/image-upload.component.ts
+++ image-upload/image-upload.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'image-upload',
-  templateUrl: './image-upload.component.html',
-  styleUrls: ['./image-upload.component.css']
+  templateUrl: 'image-upload.component.html',
+  styleUrls: ['image-upload.component.css']
 })
 export class ImageUploadComponent{
   file_srcs: string[] = [];
