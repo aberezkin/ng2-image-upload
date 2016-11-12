f646f6ef3acbcc7202f6f1bcb9e7f45a6d410e91
Arkadii Berezkin <arkm4n@gmail.com>
2016-11-12 22:54:28 +1000

    Scale and center images using CSS background

--- src/image-upload/image-upload.component.ts
+++ src/image-upload/image-upload.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class ImageUploadComponent{
   file_sources: string[] = [];
 
+
   private isFileOver:boolean = false;
 
   private buttonMessage: string = "Select Files";
@@ -17,49 +18,18 @@ export class ImageUploadComponent{
   fileChange(files) {
 
     for (var i = 0; i < files.length; i++) {
-
       var img = document.createElement('img');
       img.src = window.URL.createObjectURL(files[i]);
 
       var reader = new FileReader();
 
       reader.addEventListener('load', (event: any) => {
-        img.src = event.target.result;
-
-
-        var resized_img = this.resize(img);
-
-
-        this.file_sources.push(resized_img);
+        this.file_sources.push(event.target.result);
       }, false);
 
       reader.readAsDataURL(files[i]);
     }
 
-  }
-
-
-  resize (img, MAX_WIDTH:number = 86, MAX_HEIGHT:number = 86) {
-    var canvas = document.createElement('canvas');
-
-    var width = img.width;
-    var height = img.height;
-
-    if (width > height && width > MAX_WIDTH) {
-      height *= MAX_WIDTH / width;
-      width = MAX_WIDTH;
-    } else if (height > MAX_HEIGHT) {
-      width *= MAX_HEIGHT / height;
-      height = MAX_HEIGHT;
-    }
-
-    canvas.width = width;
-    canvas.height = height;
-    var ctx = canvas.getContext('2d');
-
-    ctx.drawImage(img, 0, 0, width, height);
-
-    return canvas.toDataURL('image/jpeg')
   }
 
   fileOver(isOver) {
