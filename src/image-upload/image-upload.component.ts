bb9a6427a9c50ebd70ee65c351009f5dec3691b2
Arkadii Berezkin <arkm4n@gmail.com>
2016-11-13 02:22:46 +1000

    Limit images number with [max] directive (100 by default)

--- src/image-upload/image-upload.component.ts
+++ src/image-upload/image-upload.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {max} from "rxjs/operator/max";
 
 class FileHolder {
   constructor(public src: string, public file: File) { }
@@ -10,7 +11,10 @@ class FileHolder {
   styleUrls: ['image-upload.component.css'],
 })
 export class ImageUploadComponent{
+  @Input() max: number = 100;
   private files: FileHolder[] = [];
+
+  private fileCounter: number = 0;
 
   private isFileOver:boolean = false;
 
@@ -19,12 +23,11 @@ export class ImageUploadComponent{
   private dropMessage: string = "Release mouse to upload";
 
   fileChange(files) {
-    for (var i = 0; i < files.length; i++) {
+    for (var i = 0; i < files.length && this.fileCounter < this.max; i++, this.fileCounter++) {
       var img = document.createElement('img');
       img.src = window.URL.createObjectURL(files[i]);
 
       var reader = new FileReader();
-
       reader.addEventListener(
         'load',
         (event: any) => this.files.push(new FileHolder(event.target.result, files[i])),
@@ -37,6 +40,7 @@ export class ImageUploadComponent{
   deleteFile(file: FileHolder): void {
     let index = this.files.indexOf(file);
     this.files.splice(index, 1);
+    this.fileCounter--;
   }
 
   fileOver(isOver) {
