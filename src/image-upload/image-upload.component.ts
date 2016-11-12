4d74941f45f405486f305d15edfbbbd13bf6065e
Arkadii Berezkin <arkm4n@gmail.com>
2016-11-13 00:28:50 +1000

    Delete image on mouse click

--- src/image-upload/image-upload.component.ts
+++ src/image-upload/image-upload.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 
+class FileHolder {
+  constructor(public src: string, public file: File) { }
+}
+
 @Component({
   selector: 'image-upload',
   templateUrl: 'image-upload.component.html',
   styleUrls: ['image-upload.component.css'],
 })
 export class ImageUploadComponent{
-  file_sources: string[] = [];
-
+  private files: FileHolder[] = [];
 
   private isFileOver:boolean = false;
 
@@ -16,20 +19,24 @@ export class ImageUploadComponent{
   private dropMessage: string = "Release mouse to upload";
 
   fileChange(files) {
-
     for (var i = 0; i < files.length; i++) {
       var img = document.createElement('img');
       img.src = window.URL.createObjectURL(files[i]);
 
       var reader = new FileReader();
 
-      reader.addEventListener('load', (event: any) => {
-        this.file_sources.push(event.target.result);
-      }, false);
+      reader.addEventListener(
+        'load',
+        (event: any) => this.files.push(new FileHolder(event.target.result, files[i])),
+        false);
 
       reader.readAsDataURL(files[i]);
     }
+  }
 
+  deleteFile(file: FileHolder): void {
+    let index = this.files.indexOf(file);
+    this.files.splice(index, 1);
   }
 
   fileOver(isOver) {
