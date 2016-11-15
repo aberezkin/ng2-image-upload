16a3aae56f991baa2f29830caf2bef266bab3b48
Arkadii Berezkin <arkm4n@gmail.com>
2016-11-16 01:27:02 +1000

    Inline template and styles (temporary workaround)
    We should create gulp script that does it before building the lib.

--- src/image-upload/image-upload.component.ts
+++ src/image-upload/image-upload.component.ts
@@ -9,8 +9,199 @@ class FileHolder {
 
 @Component({
   selector: 'image-upload',
-  templateUrl: 'image-upload.component.html',
-  styleUrls: ['image-upload.component.css'],
+  template: `
+<div class="image-upload"
+     fileDrop
+     [accept]="['image/*']"
+     (isFileOver)="fileOver($event)"
+     (fileDrop)="fileChange($event)"
+     [ngClass]="{'file-is-over': isFileOver}"
+>
+  <div class="file-upload hr-inline-group">
+    <label class="upload-button">
+      <span>{{buttonMessage}}</span>
+      <input
+        type="file"
+        accept="image/*"
+        multiple (change)="fileChange(input.files)"
+        #input>
+    </label>
+
+    <div class="drag-box-message">{{dragBoxMessage}}</div>
+  </div>
+
+  <div class="image-container hr-inline-group">
+    <div
+      class="image"
+      *ngFor="let file of files"
+      [ngStyle]="{'background-image': 'url('+ file.src +')'}"
+    >
+      <div *ngIf="file.pending" class="loading-overlay">
+        <div class="spinningCircle"></div>
+      </div>
+      <div *ngIf="!file.pending" class="x-mark" (click)="deleteFile(file)">
+        <span class="close"></span>
+      </div>
+    </div>
+  </div>
+</div>
+`,
+  styles: [
+    `
+.image-upload {
+  --active-color: #30bf5a;
+  position: relative;
+  border-radius: 5px;
+  border: #d0d0d0 dotted 2px;
+  font-family: sans-serif;
+}
+
+.file-is-over {
+  border: var(--active-color) solid 2px;
+}
+
+.hr-inline-group:after {
+  display: table;
+  clear: both;
+  content: "";
+}
+
+.file-upload {
+  padding: 15px;
+  background-color: #f3f3f3;
+}
+
+.drag-box-message {
+  float: left;
+  display: inline-block;
+  margin-left: 10px;
+  padding-top: 8px;
+  color: #9b9b9b;
+  font-weight: 600;
+}
+
+label.upload-button input[type=file]{
+  display: none;
+  position: fixed;
+  top: -99999px;
+}
+
+.upload-button {
+  cursor: pointer;
+  background-color: var(--active-color);
+  padding: 5px 10px 5px 10px;
+  border-radius: 5px;
+  color: white;
+  font-size: 1.25em;
+  font-weight: 600;
+  text-transform: uppercase;
+  display: inline-block;
+  float: left;
+}
+
+
+.image-container {
+  background-color: #f8f8f8;
+  border-radius: 0 0 5px 5px;
+}
+
+.image {
+  float: left;
+  display: inline-block;
+  margin: 5px;
+  width: 86px;
+  height: 86px;
+  background: center center no-repeat;
+  background-size: contain;
+  position: relative;
+}
+
+.x-mark {
+  width: 20px;
+  height: 20px;
+  text-align: center;
+  cursor: pointer;
+  border-radius: 2px;
+  float: right;
+  background-color: black;
+  opacity: .7;
+  color: white;
+  margin: 2px;
+}
+
+.close {
+  width: 20px;
+  height: 20px;
+  opacity: .7;
+  position: relative;
+  padding-right: 3px;
+}
+.x-mark:hover .close {
+  opacity: 1;
+}
+.close:before, .close:after {
+  border-radius: 2px;
+  position: absolute;
+  content: '';
+  height: 16px;
+  width: 3px;
+  top: 2px;
+  background-color: #FFFFFF;
+}
+
+.close:before {
+  transform: rotate(45deg);
+}
+
+.close:after {
+  transform: rotate(-45deg);
+}
+
+.loading-overlay {
+  position: absolute;
+  top: 0; left: 0; bottom: 0; right: 0;
+  background-color: black;
+  opacity: .7;
+}
+
+.spinningCircle {
+  height: 30px;
+  width: 30px;
+  margin: auto;
+  position: absolute;
+  top: 0; left: 0; bottom: 0; right: 0;
+  border-radius: 50%;
+  border: 3px solid rgba(255, 255, 255, 0);
+  border-top: 3px solid white;
+  border-right: 3px solid white;
+  -webkit-animation: spinner 2s infinite cubic-bezier(0.085, 0.625, 0.855, 0.360);
+  animation: spinner 2s infinite cubic-bezier(0.085, 0.625, 0.855, 0.360);
+}
+
+@-webkit-keyframes spinner {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes spinner {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+
+  }
+}
+`
+  ],
 })
 export class ImageUploadComponent {
   @Input() max: number = 100;
