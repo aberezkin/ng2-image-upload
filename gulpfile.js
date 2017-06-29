7f9390bfdd78aa8c1699f440ce4cfb753000cb02
Dave Pringle <david.pringle@capp.co>
2017-06-29 13:05:56 +0100

    chore(build): fix build getting stuck

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const inlineNg2Template = require('gulp-inline-ng2-template');
 
 gulp.task('build', function () {
-  gulp.src('src/**/*.ts')
+  return gulp.src('src/**/*.ts')
     .pipe(inlineNg2Template({ base: 'src', useRelativePaths: true }))
     .pipe(gulp.dest('.tmp'));
 });
