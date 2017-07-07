805132ba2e1b6f7971d5044f9f0d57479d86cd02
Dave <david.pringle@capp.co>
2017-07-07 09:48:47 +0100

    chore(demo): updated demo for header changes

--- demo/src/app/basic/basic.component.ts
+++ demo/src/app/basic/basic.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { Headers } from '../../lib/image.service';
 
 @Component({
   selector: 'basic',
   templateUrl: './basic.component.html'
 })
 export class BasicExampleComponent {
-  myHeader: Headers = new Headers({ 'Authorization': 'MyToken' });
+  myHeaders: { [name: string]: any } = {
+    'Authorization': 'MyToken',
+    'Another Header': 'AnotherValue'
+  };
 }
