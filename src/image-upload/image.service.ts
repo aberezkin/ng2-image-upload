d57a37512b020f0449d380c06f4a410bb9d76576
Kang <jkang@veeder.com>
2018-02-12 16:00:43 +0800

    修正在IE下不能初始化显示图片列表以及按钮背景色问题

--- src/image-upload/image.service.ts
+++ src/image-upload/image.service.ts
@@ -5,6 +5,23 @@ import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class ImageService {
   constructor(private http: Http) {
+     /*
+    Here it is important to note that the Internet explorer browser must enter the catch, execute inside can perform
+     file = new File([fileBlob], fileUrl); 
+    */
+    const YaunlaideFile = (<any>window).File;
+    (<any>window).File = function (chunks1, filename1, opts1 = {}) {
+        try {
+            return function () { YaunlaideFile.call(this, chunks1, filename1); };
+        } catch (error) {
+            return function () {
+                Blob.call(this, chunks1, opts1);
+                (<any>this).lastModifiedDate = new Date();
+                (<any>this).lastModified = + (<any>this).lastModifiedDate;
+                (<any>this).name = filename1;
+            };
+        }
+    };
   }
 
   public postImage(url: string, image: File, headers?: Headers | { [name: string]: any }, partName: string = 'image', customFormData?: { [name: string]: any }, withCredentials?: boolean): Observable<Response> {
